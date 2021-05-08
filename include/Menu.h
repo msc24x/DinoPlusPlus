c0214cde2a27507b62c2f032bba2fb688af7beca
msc24x <51400654+msc24x@users.noreply.github.com>
2021-05-08 18:21:58 +0530

    Add files via upload

--- include/Menu.h
+++ include/Menu.h
@@ -1,29 +1,31 @@
-#ifndef MENU_XTRA_H
-#define MENU_XTRA_H
-#include "DATAS.h"
+#ifndef Menu_H
+#define Menu_H
 
-class Menu_xtra
+
+class Menu
 {
-    private:
-        int SCREEN_HIEGHT, SCREEN_WIDTH;
-        bool selected = 0;
-        int pointingTo = 1;
-        int keyPressed;
-        int nofOptions;
-        int nofsubOptions;
-        int pointingToSubOpt = 1;
-    public:
-        void selectSubMech(int n);
-        void selectMech(int n, int Align);
-        void hideCursor();
-        void placeCursor(short int x,short int y);
-        void cls();
-        void printTitle();
+private:
+	//int SCREEN_HIEGHT, SCREEN_WIDTH;
+	bool selected = 0;
+	int pointingTo = 1;
+	int keyPressed;
+	int nofOptions;
+	int nofsubOptions;
+	int pointingToSubOpt = 1;
+public:
+	void selectSubMech(int n);
+	void selectMech(int n, int Align);
+	/*void hideCursor();
+	void placeCursor(short int x, short int y);
+	void cls();
+	void printTitle();
 
-        void mainMenu(int , int);
-            void settings();
-            void help();
-            void quit();
+	void resetGame();*/
+
+	void mainMenu(int, int);
+	void settings();
+	void help();
+	void quit();
 };
 
-#endif // MENU_XTRA_H
+#endif // Menu_H
